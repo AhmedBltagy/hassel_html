46f456f93aee9db4d1e211fe85f9f470f3a1d363
moaz <moaz.a.gad@gmail.com>
2023-12-09 01:02:13 +0400

    add login and sign up pages and start user pages

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+// lang
+document.addEventListener("DOMContentLoaded", (event) => {
+  document.getElementById("lang-toggle").addEventListener("click", function () {
+    this.textContent = this.textContent === "EN" ? "ع" : "EN"
+  })
+})
+
 function updateProgressBar() {
   var progressBar = document.getElementById("progressBar")
   var days = document.getElementById("days")
@@ -20,3 +27,28 @@ function updateProgressBar() {
 }
 
 updateProgressBar()
+
+// Login and forget password
+
+function switchInput(inputType) {
+  var idNumberField = document.getElementById("id-number-field")
+  var usernameField = document.getElementById("username-field")
+  var idNumberTab = document.getElementById("id-number-tab")
+  var usernameTab = document.getElementById("username-tab")
+  idNumberField.style.display = "none"
+  usernameField.style.display = "none"
+
+  if (inputType === "id") {
+    idNumberField.style.display = "block"
+    idNumberTab.classList.add("btn-main")
+    idNumberTab.classList.remove("light-bg")
+    usernameTab.classList.remove("btn-main")
+    usernameTab.classList.add("light-bg")
+  } else {
+    usernameField.style.display = "block"
+    usernameTab.classList.add("btn-main")
+    usernameTab.classList.remove("light-bg")
+    idNumberTab.classList.remove("btn-main")
+    idNumberTab.classList.add("light-bg")
+  }
+}
